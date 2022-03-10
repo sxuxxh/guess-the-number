5247fc44c110641bd533791aab67873f45a46f1d
Xiong (Shawn) He <sxuxxh@yahoo.com>
2022-03-09 21:52:28 -0500

    add exception handling, refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,15 @@ Project2 - Guess the Number
 import java.util.Scanner;
 
 public class Main {
-    private static final String greetingMsg = "Hello! What is your name? ";
-    private static final String askGuess = "Well, %s, I m thinking of a number between 1 and 20.\n" +
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String greetingMsg = "Welcome to Guess The Number! What is your name? ";
+    private static final String askGuess = "Well, %s, I m thinking of a number between 1 and 20." +
             "Take a guess. You have six tries for the game, good luck!\n ";
     private static final int randNumFrom = 1;
     private static final int randNumTo = 20;
+    private static final String quitOrNot = "Enter \"q\" to quit the game and any other key to continue.";
+    private static final String badName = "Wrong input, please enter your name.";
+    private static final String badGuess = "Wrong input, please enter a number between 1 and 20.";
 
     public static void main(String[] args) {
         final String urGuess = "Your guess: ";
@@ -17,53 +21,77 @@ public class Main {
         final String tooHi = " is too high, try again.";
         final String lost = "Game over, you lost.\n";
         final String win = " is correct. You win the game!!";
-        final String again = "try again";
+        final String exMsg1 = "Something is wrong with your input. Here is the stacktrace...";
+        final String bye = "Goodbye, see you again!";
 
-        //get a random number
-        int randNum = getRandNum();
-        //for testing only
-        System.out.println(randNum);
-
-        //User greeting and get inputs
+        //User greeting and get name
         //Max 6 tries for a game
-        Object usrAns = getUserInput(greetingMsg);
-        if (usrAns instanceof String) {
-            usrAns = (int)getUserInput(strInterpo(askGuess, (String) usrAns));
-            for (int i = 0; i < 6; i++) {
-                if ((int)usrAns == randNum) {
-                    System.out.println(urGuess + usrAns + win);
-                    break;
-                } else if ((int)usrAns < randNum) {
-                    if (i == 5) {
-                        System.out.println(lost);
+        String usrName = getUserName();
+
+        do {
+            //get a random number
+            int randNum = getRandNum();
+            //print it for testing only
+            System.out.println("The secret number is: " + Integer.toString(randNum));
+
+            System.out.println(strInterpo(askGuess, usrName));
+            try {
+                int usrGuess = Integer.parseInt(getGuess());
+                for (int i = 0; i < 6; i++) {
+                    if (usrGuess == randNum) {
+                        System.out.println(urGuess + usrGuess + win);
                         break;
+                    } else if (usrGuess < randNum && usrGuess != -1) {
+                        if (i == 5) {
+                            System.out.println(lost);
+                            break;
+                        }
+                        System.out.println(urGuess + usrGuess + tooLo);
+                        usrGuess = Integer.parseInt(getGuess());
+                    } else if ((usrGuess > randNum && usrGuess != -1)) {
+                        if (i == 5) {
+                            System.out.println(lost);
+                            break;
+                        }
+                        System.out.println(urGuess + usrGuess + tooHi);
+                        usrGuess = Integer.parseInt(getGuess());
                     }
-                    System.out.println(urGuess + usrAns + tooLo);
-                    usrAns = (int)getUserInput(again);
-                    continue;
-                } else if ((int)usrAns > randNum) {
-                    if (i == 5) {
-                        System.out.println(lost);
-                        break;
-                    }
-                    System.out.println(urGuess + usrAns + tooHi);
-                    usrAns = (int)getUserInput(again);
-                    continue;
                 }
+            } catch (Exception e) {
+                System.out.println(exMsg1);
+                e.printStackTrace();
             }
+        } while (!stayOrQuit());
+        System.out.println(bye);
+    }
+
+    //Method: continue to play or exit
+    public static boolean stayOrQuit() {
+        System.out.println(quitOrNot);
+        String quit = scanner.next();
+        return quit.equals("q");
+    }
+
+    //Method: get user name
+    public static String getUserName() {
+        System.out.println(greetingMsg);
+        try {
+            return scanner.nextLine();
+        } catch (Exception e) {
+            System.out.println(badName);
+            e.printStackTrace();
+            return "-1";
         }
     }
 
-    //Method: get user input and return an object
-    // based on the challenge message/ask string
-    public static Object getUserInput(String askStr) {
-        Scanner scanner = new Scanner(System.in);
-        if (askStr.equals(greetingMsg)) {
-            System.out.println(greetingMsg);
-            return scanner.nextLine();
-        } else {
-            System.out.println(askStr);
-            return scanner.nextInt();
+    //Method: get user guess number as string
+    public static String getGuess() {
+        try {
+            return scanner.next();
+        } catch (Exception e) {
+            System.out.println(badGuess);
+            e.printStackTrace();
+            return "-1";
         }
     }
 
